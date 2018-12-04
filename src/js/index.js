38a71ae138ec6ce350f6373d24c2877dbed50ccd
Janith Olagama <janitholagama.info@gmail.com>
2018-12-04 22:44:29 +0400

    Add event listener to take the input from search field
    "submit" event added with a callback function to control the search

--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,12 @@ import Search from "./model/Search"
 
 const search = new Search ("pizza");
 console.log (search);
+//Event listner to handle the input query
+document.querySelector (".search").addEventListener ("submit", e => {
+    //Prevent page reload on pressing the submit button
+    e.preventDefault ();
+    controlSearch ();
+});
 
 search.getRecipe ();
 
