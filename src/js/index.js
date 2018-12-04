1d37b0b5899ba873010ccb189b545beb93289d4f
Janith Olagama <janitholagama.info@gmail.com>
2018-12-04 20:05:53 +0400

    Make the API with axios
    Install axios NPM module
    Make the API call with Axios and get the response from food2fork api

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,19 @@
+import axios from "axios";
+
 // Global app controller
+
+
+//API URL : https://www.food2fork.com/api/search?key=YOUR_API_KEY&q=chicken%20breast
+
+async function getRecipe(searchQuery) {
+    const key = "d92c6d78b1a77713feb40ee412b02dd7";
+    try {
+        const recipeSearch = await axios(`https://www.food2fork.com/api/search?key=${key}&q= ${searchQuery}`);
+        const recipes = recipeSearch.data.recipes;
+        console.log(recipes);
+    } catch (error) {
+        console.log (error);
+    }
+};
+
+getRecipe("chicken%20breast");
